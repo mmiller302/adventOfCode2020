796a7020d0773963b60a057a8a0e110ab84c5305
mmiller302 <75397001+mmiller302@users.noreply.github.com>
2020-12-03 15:08:45 -0700

    Create Day3.py

---
+++ Day3/Day3.py
@@ -0,0 +1,30 @@
+#this is pretty ugly but it works
+
+r = tree_map.splitlines()
+
+def trees(right,down):
+    num_trees = 0
+    start = 0
+    nline = ""
+    for i in range(0,len(r),down):
+        while i < len(r)-(down+1):
+            if i == 0:
+                fline = r[i+1]
+            else:
+                fline = nline
+            fidx = r.index(fline)
+            nline = r[fidx+down]
+        
+            if start >= (len(fline) - right):
+                fline = nline
+                start = right - (len(fline) - start)
+                move_down = fline[start]                
+            else:
+                line_start = fline[start]
+                start += right
+                move_right = fline[start]
+                move_down = nline[start]
+            if '#' in move_down:
+                    num_trees += 1
+            break
+    print('Slope is R%s, D%s. Trees = %s' % (right,down,num_trees))
