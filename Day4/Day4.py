88243e821c43ce77f2371b2f78b47518a834cf1c
mmiller302 <75397001+mmiller302@users.noreply.github.com>
2020-12-05 13:15:09 -0700

    Update Day4.py

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -6,8 +6,8 @@ def get_input():
     return input
 
 fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-hcl_fields = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+hcl = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+eye_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
 def check_passports(validation=False):    
     data = get_input().split('\n\n')
@@ -40,12 +40,16 @@ def policy_check(data_entry):
             field = (data_entry[a]).split(':')[0]
             value = (data_entry[a]).split(':')[1]
             if field in fields:
+                # Birth Year
                 if 'byr' in field and len(value) == 4 and int(value) >= 1920 and int(value) <= 2002:
                     valid_fields.append(field)
+                # Issue Year
                 elif 'iyr' in field and len(value) == 4 and int(value) >= 2010 and int(value) <= 2020:
                     valid_fields.append(field)
+                # Expiration Year
                 elif 'eyr' in field and len(value) == 4 and int(value) >= 2020 and int(value) <= 2030:
                     valid_fields.append(field)
+                # Height
                 elif 'hgt' in field:
                     if 'cm' in value or 'in' in value[-2:]:
                         if 'cm' in value:
@@ -54,18 +58,27 @@ def policy_check(data_entry):
                         elif 'in' in value:
                             if int(value.strip('in')) >= 59 and int(value.strip('in')) <= 76:
                                 valid_fields.append(field)
+                # Hair Color
                 elif 'hcl' in field:
                     if '#' in value[0] and len(value) == 7:
                         for s in value:
-                            if s in hcl_fields:
+                            if s in hcl:
                                 valid_hcl.append(s)
                         if len(valid_hcl) == 6:
                             valid_fields.append(field)
-                elif 'ecl' in field:
-                    if value in colors:
-                        valid_fields.append(field)
-                elif 'pid' in field and len(value) == 9:
+                # Eye Color
+                elif 'ecl' in field and value in eye_colors:
                     valid_fields.append(field)
+                # Passport ID
+                elif 'pid' in field and len(value) == 9:
+                    #check that pid actually has ints 0-9 only
+                    nums = [num for num in range(10)]
+                    nums_in_pid = []
+                    for n in value:
+                        if int(n) in nums:
+                            nums_in_pid.append(n)
+                    if len(nums_in_pid) == 9:
+                        valid_fields.append(field)
         if len(valid_fields) == len(fields):
             return True
     return False
